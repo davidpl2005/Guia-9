b0ab67f1aa013edbf364a3fcbda2a1c514cd75c3
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-23 20:21:44 -0500

    Spinner

---
+++ guia_9/src/jspinner/Jspinner.java
@@ -0,0 +1,21 @@
+
+package jspinner;
+
+/**
+ *
+ * @author isac pacheco
+ */
+public class Jspinner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+        
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+    }
+    
+}
